aa25e5ad44ccb96e236f1a952277777734afbe4d
emmabeynon <emma.beynon@gmail.com>
2016-02-09 16:22:47 +0000

    Added before_install scripts to travis.yml - brew install node; npm install -g bower; npm install -g ios-sim.  Amended karma.conf.js files.

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -15,7 +15,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'test/**/*.spec.js',
+      '**/*.spec.js',
       'www/js/**/*.js'
     ],
 
